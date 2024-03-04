056a823be867edfcaacac2da44746fcf06c1ba50
makerspacesite <160186391+makerspacesite@users.noreply.github.com>
2024-03-04 14:01:10 -0700

    Added ESLint, fixed style, and added a .gitignore
    .gitignore prevents ESLint files from being put on github, and I fixed the style as ESLint told me to.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+//ESLint doesn't realize $ is declared, so, this.
+// eslint-disable-next-line no-undef
 $('img').mousedown(function (e) {
-    if(e.button == 2) { // right click
-      return false; // do nothing!
-    }
-  });
+	if(e.button == 2) { // right click
+		return false; // do nothing!
+	}
+});
