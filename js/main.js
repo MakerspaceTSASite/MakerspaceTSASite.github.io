cf404f93eaefa9ae61422bd8295de8acb53dbfa8
makerspacesite <160186391+makerspacesite@users.noreply.github.com>
2024-03-20 10:46:33 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 //ESLint doesn't realize $ is declared, so, this.
 // eslint-disable-next-line no-undef
 $('img').mousedown(function (e) {
-	if(e.button == 2) { // right click
-		return false; // do nothing!
+	if(e.button == 2) {
+		return false;
 	}
 });
 // eslint-disable-next-line no-undef
 $('body').mousedown(function (e) {
-	if(e.button == 2) { // right click
-		return false; // do nothing!
+	if(e.button == 2) { 
+		return false;
 	}
 });
