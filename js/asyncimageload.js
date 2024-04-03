d8f401eb9c54aabdad16e6084687501be2c77f22
makerspacesite <160186391+makerspacesite@users.noreply.github.com>
2024-04-03 16:19:36 -0600

    Improve loading times

--- js/asyncimageload.js
+++ js/asyncimageload.js
@@ -1,20 +1,48 @@
 (() => {
 	'use strict';
-	// Page is loaded
-	const objects = document.getElementsByClassName('asyncImage');
-	Array.from(objects).map((item) => {
-		// Start loading image
-		const img = new Image();
-		console.log(img);
-		img.src = item.dataset.src;
-		console.log(img.src);
-		// Once image is loaded replace the src of the HTML element
-		img.onload = () => {
-			item.classList.remove('asyncImage');
-			console.log(item.nodeName === 'IMG' ? 'It is equal IMG' : 'It does not equal IMG');
-			return item.nodeName === 'IMG' ? 
-				item.src = item.dataset.src :        
-				item.style.backgroundImage = `url(${item.dataset.src})`;
+	// Wait for the page to fully load
+	window.onload = () => {
+		const images = document.querySelectorAll('.asyncImage');
+		const config = {
+			rootMargin: '0px 0px 50px 0px', // Adjust the rootMargin to control when the images start loading
+			threshold: 0.01
 		};
-	});
+
+		const observer = new IntersectionObserver((entries, observer) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					loadImage(entry.target, observer);
+				}
+			});
+		}, config);
+
+		// Function to load the image
+		function loadImage(target, observer) {
+			const img = new Image();
+			img.src = target.dataset.src;
+			img.onload = () => {
+				target.classList.remove('asyncImage');
+				if (target.nodeName === 'IMG') {
+					target.src = target.dataset.src;
+				} else {
+					target.style.backgroundImage = `url(${target.dataset.src})`;
+				}
+				observer.unobserve(target);
+			};
+		}
+
+		// Start observing each image
+		images.forEach(image => {
+			observer.observe(image);
+		});
+
+		// Load images after a 10-second delay if the user has not moved the viewport
+		setTimeout(() => {
+			images.forEach(image => {
+				if (image.classList.contains('asyncImage')) {
+					loadImage(image, observer);
+				}
+			});
+		}, 10000); // 10 seconds delay
+	};
 })();
