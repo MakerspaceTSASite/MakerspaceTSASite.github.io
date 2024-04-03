df9781140746fec7d838ea1282510e56466c589c
makerspacesite <160186391+makerspacesite@users.noreply.github.com>
2024-04-03 11:12:35 -0600

    Added

--- js/main.js
+++ js/main.js
@@ -26,3 +26,20 @@ addEventListener('resize', () => {
 	// eslint-disable-next-line no-undef
 	$(document.body).css({'width':window.innerWidth});
 });
+(() => {
+	'use strict';
+	// Page is loaded
+	const objects = document.getElementsByClassName('asyncImage');
+	Array.from(objects).map((item) => {
+	// Start loading image
+		const img = new Image();
+		img.src = item.dataset.src;
+		// Once image is loaded replace the src of the HTML element
+		img.onload = () => {
+			item.classList.remove('asyncImage');
+			return item.nodeName === 'IMG' ? 
+				item.src = item.dataset.src :        
+				item.style.backgroundImage = `url(${item.dataset.src})`;
+		};
+	});
+})();
